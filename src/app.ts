64037993a1ddae968d0093063b761d6ee94dbd8e
paulohenriq-dev <paulo.silva@sof.to>
2022-06-11 18:44:45 -0300

    ajustes o módulo health

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.json());
 
+// Rotas
 app.use(healthRouter)
-
 app.use(equipRouter)
 
 app.listen(3333, () => {console.log('Servidor rodando!')});
--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,9 +1,11 @@
 import { BaseService } from '../common/base.service'
 
-export class HealthService implements BaseService {
+class HealthService implements BaseService {
     async execute(): Promise<{ healthCheck: boolean}>{
         return {
             healthCheck: true
         }
     }
 }
+
+export default new HealthService();

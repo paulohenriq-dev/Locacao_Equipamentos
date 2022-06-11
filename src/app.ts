180433930e9262d3c70393372e5285c26cf4e61a
paulohenriq-dev <paulo.silva@sof.to>
2022-06-11 19:56:01 -0300

    estrutura inicial para o módulo equipments

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { healthRouter } from './health/health.routes'
-import { equipRouter } from "../src/equipaments/equipaments.router";
+import { equipmentsRouter } from './equipments/equipments.routes'
 
 const app = express();
 
 app.use(express.json());
 
 // Rotas
-app.use(healthRouter)
-app.use(equipRouter)
+app.use(healthRouter);
+app.use('/equipments', equipmentsRouter);
 
 app.listen(3333, () => {console.log('Servidor rodando!')});
